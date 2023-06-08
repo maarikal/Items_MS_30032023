d48ad33010c194425de5ee8f9eec0474d35735a2
maarikal <maarika.lukk@khk.ee>
2023-06-08 15:57:37 +0300

    WIP Add xml support second time and fix swagger.yaml bugs [WIP #185346643]

--- backend/functions.ts
+++ backend/functions.ts
@@ -1,7 +1,7 @@
 import express, {NextFunction, Request, Response} from "express";
 import {PrismaClient} from '@prisma/client';
 import {IRequestWithSession} from 'function';
-import xml2js from 'xml2js';
+import xml2js, {Builder, parseStringPromise} from 'xml2js';
 import itemsRoutes from "./routes/itemsRoutes";
 
 const prisma = new PrismaClient();
@@ -50,20 +50,24 @@ async function authorizeRequest(req: IRequestWithSession, res: Response, next: N
     next()
 }
 
-function sendResponse(req: IRequestWithSession, res: Response, data: any) {
+export function sendResponse(req: IRequestWithSession, res: Response, data: any, status: number) {
     const acceptHeader = req.headers.accept || '';
     if (acceptHeader === 'application/json') {
         return res.status(201).json(data);
-    } else if (acceptHeader === 'text/html') {
-        return res.status(201).send(`<h1>${data.name}</h1><p>${data.description}</p>`);
-    } else if (acceptHeader === 'application/xml') {
+    } else if (
+        acceptHeader.includes('application/xml') ||
+        acceptHeader.includes('text/xml') ||
+        acceptHeader.includes('application/xhtml+xml')
+    ) {
         const xml = `
+            <?xml version="1.0" encoding="UTF-8"?>
             <item>
                 <name>${data.name}</name>
                 <description>${data.description}</description>
                 <image>${data.image}</image>
             </item>
         `;
+        res.set('Content-Type', 'application/xml');
         return res.status(201).send(xml);
     } else {
         return res.status(406).send('Not Acceptable');
@@ -71,29 +75,60 @@ function sendResponse(req: IRequestWithSession, res: Response, data: any) {
 }
 
 
-export const xmlResponse = (res: Response, object: any, status: number) => {
-    const builder = new xml2js.Builder();
-    const xml = builder.buildObject(object);
-    res.set('Content-Type', 'application/xml');
-    res.status(status).send(xml);
-};
-
-export const isXMLHeader = (req: Request) => {
+/*export const isXMLHeader = (req: Request) => {
     const acceptHeader = req.headers.accept || '';
     //const items = prisma.item.findMany();
     return acceptHeader.includes('application/xml') || acceptHeader.includes('text/xml');
-};
+};*/
 
-export const sendRequest = (req: IRequestWithSession, res: Response, data: any, status: number) => {
-    if (req.headers.accept === 'text/xml') {
+/*export const xmlResponse = (res: Response, data: any, status: number) => {
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(data);
+    res.set('Content-Type', 'application/xml');
+    res.status(status).send(xml);
+};*/
 
-        const convert = require('xml2js');
-        const options = {compact: true, ignoreComment: true, spaces: 4};
-        const xml = convert.json2xml(data, options);
-        return res.status(status).send(xml)
+export const sendRequest = async (req: IRequestWithSession, res: Response, data: any, status: number) => {
+    if ((req.headers["content-type"] === 'text/xml') || (req.headers["content-type"] === 'application/xml') || (req.headers["content-type"] === '?xml')) {
+
+        //const convert = require('xml2js').default;
+        //const options = {compact: true, ignoreComment: true, spaces: 4};
+        /* const xml = convert.json2xml(data, options);*/
+        const {name, description, image} = req.body.head.root[0];
+        /*const convert = new Builder({ headless: true, rootName: undefined });
+        console.log("data: ", req.body)
+        const xml = convert.buildObject({name, description, image});
+        console.log("xml: ", xml)
+        return res.status(status).send(xml)*/
+        const convert = new Builder({headless: true});
+        const temporaryRootName = 'tempRoot'; // Temporary root element name
+        const xml = convert.buildObject({[temporaryRootName]: {name, description, image}});
+        const finalXml = xml.replace(`<${temporaryRootName}>`, '').replace(`</${temporaryRootName}>`, ''); // Remove the temporary root element
+        console.log('xml: ', finalXml);
+        try {
+            const jsonData = await parseStringPromise(finalXml, {explicitArray: false}); // Convert finalXml to JSON
+            console.log('jsonData: ', jsonData);
+
+            return res.status(status).json(jsonData);
+        } catch (error) {
+            console.error('Error parsing XML:', error);
+            return res.status(status).json(data);
+        }
+
     }
     return res.status(status).json(data)
 }
 
+/*export const sendRequest = (req: IRequestWithSession, res: Response, data: any, status: number): void | Response<any, Record<string, any>> => {
+    const { accept } = req.headers;
+    if (accept === 'text/xml' || accept === 'application/xml') {
+        const convert = require('xml2js');
+        const options = { compact: true, ignoreComment: true, spaces: 4 };
+        const xml = convert.json2xml(data, options);
+        return xmlResponse(res, xml, status);
+    } else {
+        return res.status(status).json(data);
+    }
+};*/
 
 export default authorizeRequest;
