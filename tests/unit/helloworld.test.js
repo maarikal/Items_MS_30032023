299ced5e9b5293e2a7542740bc05dda679d456b6
Silver Sirp <silver.sirp@khk.ee>
2023-06-16 08:37:32 +0300

    WIP jest and babel

---
+++ tests/unit/helloworld.test.js
@@ -0,0 +1,9 @@
+// helloworld.test.js
+
+import HelloWorld from "../../frontend/src/views/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+
+test("HelloWorld Component renders the correct text", () => {
+    const wrapper = mount(HelloWorld);
+    expect(wrapper.text()).toBe("Hello there!");
+});
